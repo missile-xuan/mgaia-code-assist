645d9b7ff152abf1094637a7ec4abbc5edf9d34e
gxr2215 <759071738@qq.com>
2024-10-29 15:01:09 +0800

    改造注释逻辑

--- src/vue/generateComments.ts
+++ src/vue/generateComments.ts
@@ -4,6 +4,7 @@
  * 抽取props 和 emit
  */
 import * as vscode from 'vscode'
+import dayjs from 'dayjs'
 export default vscode.commands.registerCommand(
   'mgaia-code-assist.generateComments',
   () => {
@@ -20,7 +21,7 @@ export default vscode.commands.registerCommand(
     // 获取全部文本
     const word = document.getText()
     // 解析出的props
-    let props = null
+    let props: null | string[] = null
     // 通过正则 拆出props 先贪婪捕获
     const propsReg = /defineProps.*\)/s
     const propsCodeBlock = word.match(propsReg)
@@ -47,30 +48,13 @@ export default vscode.commands.registerCommand(
         props = checkJs(propsCodeBlock[0])
       }
     }
-    // js ts的注释方式
-    // let annotate = '/**\n'
-    // annotate += ` * 文件名: ${document.fileName}\n`
-    // if (props !== null) {
-    //   annotate += ' * props: \n'
-    //   const propsArr = props.split('\n')
-    //   propsArr.forEach((item) => {
-    //     annotate += ` *   ${item}\n`
-    //   })
-    // }
-    let annotate = '<!-- '
-    annotate += `  文件名: ${document.fileName}\n`
-    if (props !== null) {
-      annotate += '  props: \n'
-      const propsArr = props.split('\n')
-      propsArr.forEach((item) => {
-        if (item.trim() === '') return
-        annotate += `  ${item}\n`
-      })
-    }
-    annotate += '-->\n'
 
+    // 提取现有注释 如果存在 采用补充模式
+    const existingNote = word.match(/<!--[\s\S]*?-->/s)
+    const nowNote = existingNote !== null ? existingNote[0] : ''
+    const note = buildNote(nowNote, props)
     void editor.edit((editBuilder) => {
-      editBuilder.insert(new vscode.Position(0, 0), annotate)
+      editBuilder.insert(new vscode.Position(0, 0), note)
       void vscode.window.showInformationMessage(
         'mgaia-code-assist.generateComments: 注释生成成功！'
       )
@@ -82,41 +66,93 @@ export default vscode.commands.registerCommand(
  * 检查是否是ts写法
  * @param props
  */
-function checkTs (word: string): null | string {
+function checkTs (word: string): null | string[] {
   const reg = /<.*>/s
   const result = word.match(reg)
   if (result === null) return null
-  let props = result[0].substring(result[0].indexOf('{') + 1, result[0].length)
-  // 清理单行注释
+  const props = result[0].substring(result[0].indexOf('{') + 1, result[0].lastIndexOf('}'))
+  // 清理单行注释 以及空行
   const propsArray = props.split('\n')
-  for ()
+  const cleanCommasPropsArray = []
+  for (let i = 0; i < propsArray.length; i++) {
+    propsArray[i] = propsArray[i].trim()
+    if (propsArray[i].startsWith('//') || propsArray[i] === '') {
+      propsArray.splice(i, 1)
+      i--
+    } else {
+      cleanCommasPropsArray.push(...propsArray[i].split(',').filter((item) => item.trim() !== ''))
+    }
+  }
+  // 冒号分隔整理
+  const returnProps: string[] = []
+  cleanCommasPropsArray.forEach((item) => {
+    if (item.indexOf(':') < 1) return
 
-  props = props.substring(0, props.lastIndexOf('}'))
-  props.trim()
-  while (props[0] === '\n') {
-    props = props.substring(1, props.length)
-  }
-  while (props[props.length - 1] === '\n') {
-    props = props.substring(0, props.length - 1)
-  }
-  return props
+    const itemArray = item.trim().split(':')
+    returnProps.push(itemArray[0].trim())
+  })
+
+  return returnProps
 }
 
 /**
  * 检查是否是js写法
  * @param props
  */
-function checkJs (word: string): null | string {
+function checkJs (word: string): null | string[] {
   const reg = /\(\{.*\}\)/s
   const result = word.match(reg)
   if (result === null) return null
-  let props = result[0].substring(result[0].indexOf('(') + 1, result[0].lastIndexOf(')'))
-  props.trim()
-  while (props[0] === '\n') {
+  let props = result[0].substring(result[0].indexOf('(') + 1, result[0].lastIndexOf(')')).trim()
+
+  // 清理首尾{}
+  if (props[0] === '{') {
     props = props.substring(1, props.length)
   }
-  while (props[props.length - 1] === '\n') {
+  if (props[props.length - 1] === '}') {
     props = props.substring(0, props.length - 1)
   }
-  return props
+  // 清理内部{}
+  let leftIndex = props.indexOf('{')
+  let rightIndex = props.indexOf('}')
+  while (leftIndex !== -1) {
+    props = props.substring(0, leftIndex) + props.substring(rightIndex + 1, props.length)
+    leftIndex = props.indexOf('{')
+    rightIndex = props.indexOf('}')
+  }
+
+  // 提取字段
+  const propsArray = props.split('\n')
+  const returnProps: string[] = []
+  propsArray.forEach((item) => {
+    const itemStr = item.split(':')[0].trim()
+    if (itemStr === '' || itemStr.startsWith('//')) return
+    returnProps.push(item.split(':')[0].trim())
+  })
+  return returnProps
+}
+
+/**
+ * 构建注释
+ * @param nowNote 当前注释
+ * @param props 收集到的props参数
+ */
+function buildNote (nowNote: string, props: null | string[]): string {
+  let note = ''
+  // 如果当前没有注释 则生成注释
+  if (nowNote === '') {
+    note = '<!--\n'
+    note += `====={${dayjs().format('YYYY-MM-DD HH:mm:ss')}}=====\n`
+    if (props !== null) {
+      note += 'props参数:\n'
+      props.forEach((item) => {
+        note += `  ${item}:\n`
+      })
+    }
+    note += '-->\n'
+    return note
+  }
+  // 如果只有单行注释 则在现有注释前面添加注释
+  // ================  注释反写逻辑
+  return note
 }
